4faa0c5d64b65011d0d7078459b694679e901ce8
Antiavk <avk_tp@mail.ru>
2023-03-05 09:08:24 +0300

    new NGINX files

--- src/main/java/com/netology/authorization/AuthorizationApplication.java
+++ src/main/java/com/netology/authorization/AuthorizationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthorizationApplication {
+ public class AuthorizationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizationApplication.class, args);
-    }
+         SpringApplication.run(AuthorizationApplication.class, args);
+   }
 
 }
